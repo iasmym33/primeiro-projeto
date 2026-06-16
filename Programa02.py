1a7f55c3bb0eb0dff385652250877aebe0c13041
iasmym33 <aiasmym33@gmail.com>
2026-06-15 21:28:54 -0300

    Entrada de dados pelo terminal e operações matemáticas

---
+++ Programa02.py
@@ -0,0 +1,12 @@
+nome = input("digite seu nome: ")
+nome_amigo = input("Digite o nome do seu melhor amigo: ")
+print(f"Olá {nome} seja bem vindo ao meu programa!")
+ano_nascimento = int( input(f"Prazer em te conhecer {nome}!, \n digite o ano do seu nascimento: "))
+idade = 2026 - ano_nascimento
+print(f"Parabéns por seus {idade} anos")
+print(f"O seu melhor amigo é o {nome_amigo}")
+
+
+
+
+
---
+++ Programa03.py
@@ -0,0 +1,23 @@
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo número: "))
+
+Soma = num1 + num2
+print(f"A soma de {num1} e {num2} é igual a = {Soma}")
+
+Subtração = num1 - num2
+print(f"a subtração de {num1} e {num2} é igual a = {Subtração}")
+
+Multiplicação = num1 * num2
+print(f"A multiplicação de {num1} por {num2} é igual a {Multiplicação}")
+
+Divisão = num1 / num2
+print(f"A divisáo do {num1} por {num2} é igual a = {Divisão}")
+
+Divisão_inteira = num1 // num2
+print(f"A divisão de {num1} por {num2} é igual a = {Divisão_inteira}")
+
+resto_divisao = num1 % num2
+print(f"A divisão de {num1} e {num2} resta = {resto_divisao}")
+
+potenciacao = num1 ** num2
+print(f"A potenciação de {num1} e {num2} é igual a = {potenciacao}")
