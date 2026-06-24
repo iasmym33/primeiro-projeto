327b61c248a509727c21e75dbaf5a24099f0a295
iasmym33 <aiasmym33@gmail.com>
2026-06-23 21:22:31 -0300

    Aprendendo For e While

--- Atividade_for+while/Validador de senhas.py
+++ Atividade_for+while/Validador de senhas.py
@@ -1,9 +1,11 @@
 senha_correta = 123456
 
-for i in range(3):
+for i in range(2,0,-1):
     senha_digitada = int(input("Digite sua senhas: "))
     if senha_digitada == senha_correta:
         print("Acesso permitido")
         break
     else:
+        print(f"Senha incorreta, você tem mais {i} tentativas")
+else:
         print("conta bloqueada")
